eb3ec468291032c2b1f7243c53f83b5e4c7816ca
Michael Huang <myh2002@users.noreply.github.com>
2017-05-01 13:50:54 -0700

    Add files via upload

---
+++ jobdetails.js
@@ -0,0 +1,54 @@
+{
+  "BrandTemplateId": 0,
+  "Categories": [
+    "Part-time"
+  ],
+  "JobDescriptionText": "General Manager To assist the Management Team in increasing customer count, customer satisfaction and control costs while increasing profitability in the daily operations of an entire Burger King restaurant The team member position supports service by working a variety of positions to include, but not limited to drive thru, cashier, food production, and washing dishes. Days, evenings and weekends; flexible, part time for all shifts Please click APPLY NOW to submit your application online Additional Info Minimum Age 16+ years old Additional Must be 16 years of age or older. Good oral communication and interpersonal skills. Professional personal appearance. Ability to interpret a variety of instructions furnished in written, oral, diagram or schedule form. Ability to use a computer and calculator. Ability to work flexible hours. Ability to work days, evenings and weekends. Job Benefits Health insurance is available. Job Industries Food & Restaurant",
+  "JobDescription": "",
+  "Images": [
+    "https://maps.googleapis.com/maps/api/staticmap?center=37.505062,-77.607799&zoom=16&size=600x300&maptype=roadmap&markers=color:blue|label:S|37.505062,-77.607799"
+  ],
+  "Industries": [
+    "Food & Restaurant"
+  ],
+  "MinimumAge": 16,
+  "PostingOverviewHtml": null,
+  "PostingOverviewText": null,
+  "StandardJobTitle": "Restaurant Team Leader",
+  "FromCache": false,
+  "Self": "/v1/jobs/6098124",
+  "Elapsed": 59,
+  "AddressLine1": "11431 Midlothian Tpke",
+  "AddressLine2": "",
+  "AppGoalScore": 0,
+  "ApplicationURL": null,
+  "City": "Richmond",
+  "Company": "Burger King",
+  "Date": "2016-04-21T00:00:00-04:00",
+  "Distance": null,
+  "Features": [],
+  "ProfileModules": {
+    "AppVersionSupported": false,
+    "IsSupported": false,
+    "RequiredModules": []
+  },
+  "Id": "6098124",
+  "LogoImageUrl": null,
+  "Image": {
+    "Map": "https://maps.googleapis.com/maps/api/staticmap?center=37.505062,-77.607799&zoom=16&size=600x300&maptype=roadmap&markers=color:blue|label:S|37.505062,-77.607799",
+    "Logo": "https://media.snagajob.com/ui/app/icons/food-and-restaurant.png?v=1"
+  },
+  "IsExpired": false,
+  "IsSaved": false,
+  "IsMobileOptimized": true,
+  "JobTitle": "General Manager",
+  "Latitude": "37.505062",
+  "LocationName": "#960 - R6 - 11431 Midlothian Tpke - 1",
+  "Longitude": "-77.607799",
+  "LatLngConfidence": 0,
+  "PostalCode": "23235",
+  "SavedDate": null,
+  "StateProvCode": "VA",
+  "StateProvName": "Virginia",
+  "Value": 0.34
+};
